194ff07d6c0f2039bf246230c5e2738d5966fcc6
Elija33 <amponsaeli@gmail.com>
2023-01-05 22:30:32 -0500

    Insertion Sort

--- InsertionSort/InsertSort.java
+++ InsertionSort/InsertSort.java
@@ -1,10 +1,22 @@
 package InsertionSort;
 
+import java.util.Arrays;
+
 public class InsertSort {
     public static void Insert(int array[]){
-        
+        for(int i = 1; i < array.length; i++){
+            int key = array[i];
+            int j = i - 1;
+            while(j >= 0 && array[j] > key){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
     }
     public static void main(String[] args) {
-        
+        int array[] = {50,20,40,60,10,30};
+        Insert(array);
+        System.out.println(Arrays.toString(array));
     }
 }
--- InsertionSort/README.md
+++ InsertionSort/README.md
@@ -1,4 +1,6 @@
 # SortingCodeQustion
                                                 Insertion Sort Algorithm
-                                                
+
 Sorting Algorithm: that places an unsorted an unsorted at its suitable place in each iteration
+
+We assume that the first card is already sorted then, we select an unsorted card. If the unsorted card is greater than the card in hand, it is placed on the right otherwise, to the left. In the same way, other unsorted cards are taken and put in their right place.
